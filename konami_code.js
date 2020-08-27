358d4e31d24fcda9d2fac89de0b04cefaf39a301
kirirotha <kirirotha@gmail.com>
2020-08-27 12:16:22 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,3 +1,5 @@
+
+
 const codes = [
   "ArrowUp",
   "ArrowUp",
@@ -11,6 +13,32 @@ const codes = [
   "a"
 ];
 
+let index = 0;
+
+
+
+
 function init() {
   // your code here
+  document.body.addEventListener("keydown", (e) => {
+    onKeyDownHandler(e)
+  })
 }
+
+function onKeyDownHandler(e) {
+  const key = e.key;
+ 
+  if (key === codes[index]) {
+    index++;
+ 
+    if (index === codes.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
+
+//init();
